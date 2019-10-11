3909c09c886be50086f3b5262e976e0056b67c1a
Minos Katevas <minos.kat@gmail.com>
2019-10-11 13:04:33 +0300

    Refactor driver to new design (check README)

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@
 const https = require('https');
 const http = require('http');
 const express = require('express');
-const request = require('request');
+//const request = require('request');
 const bodyParser = require('body-parser');
+
+// DataBox
+const databox = require('node-databox');
+const DATABOX_ARBITER_ENDPOINT = process.env.DATABOX_ARBITER_ENDPOINT || 'tcp://127.0.0.1:4444';
+const DATABOX_ZMQ_ENDPOINT = process.env.DATABOX_ZMQ_ENDPOINT || 'tcp://127.0.0.1:5555';
+const DATABOX_TESTING = !(process.env.DATABOX_VERSION);
 
 // Ancile
 let AncileDefaultSettings = {};
@@ -11,69 +17,84 @@ try {
   AncileDefaultSettings = require('./ancile-secret.json');
 } catch (e) {
   AncileDefaultSettings = {
-    'server': '',
-    'token': '',
   };
 }
 
-// DataBox
-const databox = require('node-databox');
-const DATABOX_ZMQ_ENDPOINT = process.env.DATABOX_ZMQ_ENDPOINT || 'tcp://127.0.0.1:5555';
-const DATABOX_TESTING = !(process.env.DATABOX_VERSION);
 const PORT = process.env.port || '8080';
+const store = databox.NewStoreClient(DATABOX_ZMQ_ENDPOINT, DATABOX_ARBITER_ENDPOINT);
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const timer = setInterval(timer_callback, 1000 * 1);  // per second
-let next_data_refresh = null;
-
-
 app.get('/ui', function (req, res) {
   getSettings()
     .then((settings) => {
-      const { server, token } = settings;
+      // const { } = settings;
 
       res.type('html');
       res.send(`
         <h1>Ancile Driver Configuration</h1>
-        <form action="/driver-ancile/ui/saveConfiguration" >
-            Ancile Server: <input type="text" name="server" value="${server}" /><br />
-            Ancile Token: <input type="text" name="token" value="${token}" /><br />
-            <br />
-            <button>Save Configuration</button>
-        </form>
+        <p>No configuration is required at this stage.</p>
+        <p>TODO: List supported datastores and add checkboxed to grant/restrict data access</p>
       `);
     });
 });
 
-
-app.get('/ui/saveConfiguration', (req, res) => {
+app.get('/ui/getData', function (req, res) {
   getSettings()
     .then((settings) => {
-      const { server, token } = req.query;
-      settings.server = server;
-      settings.token = token;
+      // const { } = settings;
+      const { DataSourceID } = req.query;
 
-      setSettings(settings)
-        .then(() => {
-          res.type('html');
-          res.send(`
-            <h1>Ancile Driver Configuration</h1>
-            <p>Configuration has been successfully saved.</p>
-          `);
-          res.end();
-        });
+      // Check if DataSourceID is available
+      if (!DataSourceID) {
+        return Promise.reject(new Error('DataSourceID is missing'));
+      }
+
+      // read data
+      store.redditData.Read(DataSourceID).then((result) => {
+        console.log('result:', DataSourceID, result);
+        res.send('index', { data: result.value });
+      }).catch((err) => {
+        console.log('get config error', err);
+        return Promise.reject(new Error(err));
+      });
+
     });
 });
+
+
+// app.get('/ui/saveConfiguration', (req, res) => {
+//   getSettings()
+//     .then((settings) => {
+//       const { server, token } = req.query;
+//       settings.server = server;
+//       settings.token = token;
+
+//       setSettings(settings)
+//         .then(() => {
+//           res.type('html');
+//           res.send(`
+//             <h1>Ancile Driver Configuration</h1>
+//             <p>Configuration has been successfully saved.</p>
+//           `);
+//           res.end();
+//         });
+//     });
+// });
 
 
 app.get('/status', function (req, res) {
   res.send('active');
 });
 
-//const tsc = databox.NewTimeSeriesBlobClient(DATABOX_ZMQ_ENDPOINT, false);
-const kvc = databox.NewKeyValueClient(DATABOX_ZMQ_ENDPOINT, false);
+const redditData = databox.NewDataSourceMetadata();
+redditData.Description = 'Reddit Simulator data';
+redditData.ContentType = 'application/json';
+redditData.Vendor = 'Databox Inc.';
+redditData.DataSourceType = 'redditSimulatorData';
+redditData.DataSourceID = 'redditSimulatorData';
+redditData.StoreType = 'ts/blob';
 
 const driverSettings = databox.NewDataSourceMetadata();
 driverSettings.Description = 'Ancile driver settings';
@@ -83,17 +104,20 @@ driverSettings.DataSourceType = 'ancileSettings';
 driverSettings.DataSourceID = 'ancileSettings';
 driverSettings.StoreType = 'kv';
 
-kvc.RegisterDatasource(driverSettings)
+store.RegisterDatasource(redditData)
+  .then(() => {
+    return store.RegisterDatasource(driverSettings);
+  })
   .catch((err) => {
     console.log('Error registering data source:' + err);
   });
 
 function getSettings() {
-  const datasourceid = 'ancileSettings';
+  const datasourceid = 'redditSimulatorSettings';
   return new Promise((resolve, reject) => {
-    kvc.Read(datasourceid, 'settings')
+    store.KV.Read(datasourceid, 'settings')
       .then((settings) => {
-        //console.log('[getSettings] read response = ', settings);
+        console.log('[getSettings] read response = ', settings);
         if (Object.keys(settings).length === 0) {
           //return defaults
           const settings = AncileDefaultSettings;
@@ -101,11 +125,13 @@ function getSettings() {
           resolve(settings);
           return;
         }
+
         //console.log('[getSettings]', settings);
         resolve(settings);
       })
       .catch((err) => {
         const settings = AncileDefaultSettings;
+        console.log('Error getting settings', err);
         console.log('[getSettings] using defaults Using ----> ', settings);
         resolve(settings);
       });
@@ -113,11 +139,11 @@ function getSettings() {
 }
 
 function setSettings(settings) {
-  const datasourceid = 'ancileSettings';
+  const datasourceid = 'redditSimulatorSettings';
   return new Promise((resolve, reject) => {
-    kvc.Write(datasourceid, 'settings', settings)
+    store.KV.Write(datasourceid, 'settings', settings)
       .then(() => {
-        console.log('[setSettings] settings saved', settings);
+        //console.log('[setSettings] settings saved', settings);
         resolve(settings);
       })
       .catch((err) => {
@@ -127,90 +153,17 @@ function setSettings(settings) {
   });
 }
 
-// function save(datasourceid, data) {
-//   console.log('Saving Ancile event::', data.text);
-//   const json = { data };
-//   tsc.Write(datasourceid, json)
-//     .then((resp) => {
-//       console.log('Save got response ', resp);
-//     })
-//     .catch((error) => {
-//       console.log('Error writing to store:', error);
-//     });
-// }
-
-
-function timer_callback() {
-
-  getSettings()
-    .then((settings) => {
-      const { server, token } = settings;
-
-      if (!server) {
-        console.log('Warning: server property is empty.');
-        return;
-      }
-
-      if (!token) {
-        console.log('Warning: token property is empty.');
-        return;
-      }
-
-      const refresh_interval = 1; // Per 1 minute
-
-      //console.log('[timer_callback]');
-
-      // current datetime
-      var now = new Date();
-
-      if (next_data_refresh == null ||
-          next_data_refresh < now) {
-
-        refresh_data();
-
-        // plan next refresh
-        next_data_refresh = new Date().setMinutes(now.getMinutes() + refresh_interval);
-      }
+async function save(datasourceid, data) {
+  console.log('Saving Reddit Simulator event::', data);
+  const json = { data };
+  store.TSBlob.Write(datasourceid, json)
+    .then((resp) => {
+      console.log('Save got response ', resp);
+    })
+    .catch((error) => {
+      console.log('Error writing to store:', error);
     });
 }
-
-function refresh_data() {
-  getSettings()
-    .then((settings) => {
-      const { server, token } = settings;
-
-      console.log('[refresh_data]');
-
-      // build data
-      const data = {
-        token,
-        users: ['minos.tmp@gmail.com'],
-        purpose: 'research',
-        program: `
-dp_1 = user_specific["minos.tmp@gmail.com"].get_empty_data_pair(data_source='test')
-test.test_transform(data=dp_1)
-test.test_transform2(data=dp_1)
-general.flatten(data=dp_1)
-result.append_dp_data_to_result(data=dp_1)
-        `,
-      };
-
-      // make the request
-      request({
-        url: server + '/api/run',
-        method: 'POST',
-        json: data,
-      }, (err, res, body) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log(`statusCode: ${res.statusCode}`);
-        console.log(body);
-      });
-    });
-}
-
 
 //when testing, we run as http, (to prevent the need for self-signed certs etc);
 if (DATABOX_TESTING) {
@@ -218,6 +171,6 @@ if (DATABOX_TESTING) {
   http.createServer(app).listen(PORT);
 } else {
   console.log('[Creating https server]', PORT);
-  const credentials = databox.getHttpsCredentials();
+  const credentials = databox.GetHttpsCredentials();
   https.createServer(credentials, app).listen(PORT);
 }
