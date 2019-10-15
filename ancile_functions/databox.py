27e361af524313aaacdb216717a60d825b178501
Minos Katevas <minos.kat@gmail.com>
2019-10-15 15:02:12 +0100

    Add simple get_latest ancile function

---
+++ ancile_functions/databox.py
@@ -0,0 +1,20 @@
+from ancile.core.decorators import *
+from ancile.lib.general import get_token
+from ancile.utils.errors import AncileException
+
+name="databox"
+
+
+@ExternalDecorator()
+def get_latest(data_source_id):
+    import requests
+
+    url = "https://127.0.0.1/app-ancile/ui/Latest"
+    payload = {'DataSourceID': data_source_id}
+    res = requests.get(url, params=payload)
+    if res.status_code == 200:
+        data = res.json()
+    else:
+        raise AncileException("Couldn't fetch data from databox.")
+
+    return data
